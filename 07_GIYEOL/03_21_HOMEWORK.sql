af290d382d8ac0471d4fff8f780cca57f7b2c1e7
GIYEOL <bgy11178@gmail.com>
2025-03-22 23:07:05 +0900

    empdb

--- 07_GIYEOL/03_21_HOMEWORK.sql
+++ 07_GIYEOL/03_21_HOMEWORK.sql
@@ -24,18 +24,18 @@ where
     employee_mailadd
     like '____'
   and
-    DEPT_CODE = D9 or DEPT_CODE= D5
+    (DEPT_CODE = D9) or (DEPT_CODE= D5)
   between
-    employee_data = 90/01/01 or empolyee_date = 01/12/31
+    (employee_data = 90/01/01) or (empolyee_date = 01/12/31)
   and
-    employee_money >= 270;
+    (employee_money >= 270);
 -- 4
 select
     employee_name
 from
     tbl_empolyee
 where
-    employee_name = works
+    (employee_name = works)
 group by  employee_name ASC;
 -- 5
 select
@@ -47,11 +47,12 @@ from
     tbl_emplyee
 where
     employee_phonenumber
-like '%2'
+like
+    '%2'
 between
-    employee_end ='Y'
+    (employee_end = 'Y')
    or
-    employee_works ='N';
+    (employee_works ='N');
 -- 7
 select
     employee_name,
@@ -60,3 +61,43 @@ from
     tbl_employee
 where
     employee_grde not null;
+-- 02.1
+select
+    employee_num,
+    employee_name,
+    employee_id,
+    employee_money
+from
+    tbl_employee
+where
+    row_count(employee_id,*,'15');
+-- 2
+select
+    emp_name,
+    emp_id
+from
+    tbl_empdb
+where
+
+-- 3
+CREATE TABLE tbl_files (
+                           file_no BIGINT,
+                           file_path VARCHAR(500)
+);
+-- drop table tbl_files
+INSERT INTO tbl_files VALUES(1, 'c:\\abc\\deft\\salesinfo.xls');
+INSERT INTO tbl_files VALUES(2, 'c:\\music.mp3');
+INSERT INTO tbl_files VALUES(3, 'c:\\documents\\resume.hwp');
+COMMIT;
+SELECT * FROM tbl_files;
+
+-- 03.1
+select
+    emp_money ,
+    sum(*) as '급여 총합'
+from
+    tbl_empdb
+where
+    emp_sex ='woman' ;
+-- 2
+select
