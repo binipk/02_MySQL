3b786e03df43d6ba91314b7b20d4617068aed90e
juhui <juya202106@naver.com>
2025-03-25 19:23:24 +0900

    2장 다 풀고 3장도 다 풀었음

--- 12_JUHUI/003_Group_Functions.sql
+++ 12_JUHUI/003_Group_Functions.sql
@@ -24,6 +24,23 @@ SELECT
         ------------------------------
         15,760,000      745,000
     */
+-- 답안
+SELECT
+    FORMAT(sum(salary), 0) 급여총합,
+    FORMAT(sum(salary * IFNULL(bonus, 0)), 0) "보너스 총합"-- 가상컬럼
+FROM
+    employee
+WHERE
+    dept_code = 'D5';
+
+-- 내가 푸는 거
+SELECT
+    FORMAT(SUM(salary),0) AS '급여총합'
+    , FORMAT(sum(salary * IFNULL(bonus,0)),0) AS '보너스 총합'
+    FROM
+        employee
+    WHERE
+        dept_code = 'D5';
 
 
 -- 3. 부서코드가 D5인 직원의 보너스 포함 연봉의 총합 을 계산
@@ -36,6 +53,13 @@ SELECT
     */
 
 
+SELECT
+    FORMAT(SUM((salary + (salary * IFNULL(bonus,0))) * 12),0) 연봉
+    FROM
+        employee
+    WHERE
+        dept_code = 'D5';
+
 
 
 -- 4. 남/여 사원 급여합계를 동시에 표현(가공된 컬럼의 합계)
@@ -47,6 +71,10 @@ SELECT
          49,760,000                24,816,240
     */
 
+SELECT
+    FORMAT(SUM(CASE WHEN SUBSTR(emp_no,8,1) IN (1, 3) THEN salary ELSE 0 END),0 )AS '남사원 급여 합계'
+    , FORMAT(SUM(CASE WHEN SUBSTR(emp_no,8,1) IN (2, 4) THEN salary ELSE 0 END),0 )AS '여사원 급여 합계'
+    FROM employee;
 
 
 -- 5.  [EMPLOYEE] 테이블에서 사원들이 속해있는 부서의 수를 조회
@@ -60,3 +88,8 @@ SELECT
     */
 
 -- (NULL은 제외하고, 중복된 부서는 하나로 카운팅)
+
+-- 중복 제거는 DISTINCT, NULL은 제외되어 카운팅되지 않음.
+SELECT
+    COUNT(DISTINCT dept_code ) '부서 수'
+    FROM employee;
