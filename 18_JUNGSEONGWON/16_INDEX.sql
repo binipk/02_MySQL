3e3f4569befb1babb2deac12cbd0a4c2a3913972
dkrio <dkrio34@gmail.com>
2025-03-26 09:00:40 +0900

    과제5번까지 완료

--- 18_JUNGSEONGWON/16_INDEX.sql
+++ 18_JUNGSEONGWON/16_INDEX.sql
@@ -10,4 +10,13 @@ VALUES (1, 'galaxyS23', 1200000),
        (3, 'galaxyZfold3', 1730000);
 
 SELECT * FROM phone;
+SELECT * FROM phone WHERE phone_name = 'galaxyS23';
 EXPLAIN SELECT * FROM phone WHERE phone_name = 'galaxyS23';
+
+-- phone_name에 index 추가하기
+
+CREATE INDEX idx_name ON phone(phone_name);
+SELECT * FROM phone WHERE phone_name = 'galaxyS23';
+EXPLAIN SELECT * FROM phone WHERE phone_name = 'galaxyS23';
+
+DROP INDEX idx_name ON phone;
