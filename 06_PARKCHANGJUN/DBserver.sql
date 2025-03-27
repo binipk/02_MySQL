930cb448f9b1d4f6db968a104e7ea7d5e38a6e69
parkchangjun <ory01@hs.ac.kr>
2025-03-28 08:37:00 +0900

    수정

--- 06_PARKCHANGJUN/DBserver.sql
+++
@@ -1,7 +0,0 @@
-use mysql;
-show databases;
-select * from user;
-
-create database menudb;
-create user 'ohgiraffers'@'%' identified by 'ohgiraffers';
-grant all privileges on menudb.* To 'ohgiraffers'@'%';
